ff526a26fd98bf3cf5a900876db9e5890f7adda5
ING'S <92977647+Nifskor@users.noreply.github.com>
2022-05-11 04:56:00 +0900

    회원가입 페이지 1차 개발 (프론트만)

---
+++ front_web/jsp/w3.js
@@ -0,0 +1,17 @@
+// 외부에서 다른 html 호출하는 스크립트 
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.outerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
