30353b44986b91f30a3049c029b2b308119dc850
Mina-1316 <mina1316.canreply@gmail.com>
2022-05-12 20:06:53 +0900

    Feat: Authentication(Login/Logout via session cookie) add
    Authentication model(user model), its serializers, API Endpoints(Login/Logout) finished

--- backend/apiserver/api/urls.py
+++ backend/apiserver/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
-
+from rest_framework import routers
 from api import views
+
+router = routers.SimpleRouter()
+router.register(r'user', views.NormalUserManageView, basename='api')
 
 urlpatterns = [
     # User signup API URLs
     path('signup/', views.UserCreateView.as_view(), name='signup'),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('auth/', include('api.auth.urls')),
+    path('check/', views.AuthStatusView.as_view(), name='check'),
 ]
+
+urlpatterns += router.urls
--- backend/apiserver/api/views.py
+++ backend/apiserver/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.decorators import action
 
 from api.models import User
-from api.serializers import UserSerializer
+from api.serializers import UserSerializer, UserWithoutPasswordSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
@@ -11,3 +15,50 @@ class UserCreateView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class NormalUserManageView(viewsets.GenericViewSet,
+                           mixins.RetrieveModelMixin,
+                           mixins.UpdateModelMixin,
+                           mixins.DestroyModelMixin):
+    """
+    Manage the user's profile.
+    This endpoint is available only to the user himself except retrieve.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserWithoutPasswordSerializer
+    queryset = User.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        """
+        Update the user's profile.
+        This endpoint is available only to the user himself.
+        """
+        user = self.get_object()
+        if user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Delete the user's profile.
+        This endpoint is available only to the user himself.
+        """
+        user = self.get_object()
+        if user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+
+class AuthStatusView(APIView):
+    """
+    Check if the user is authenticated
+    """
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request: Request) -> Response:
+        return Response({
+            "status": "authenticated",
+            "user": request.user.email,
+            "auth": request.auth
+        })
