d35c4adecf6ffdf3c6f7f8412f3ccf744018f64e
ING'S <92977647+Nifskor@users.noreply.github.com>
2022-05-16 03:01:08 +0900

    다크모드(초기버전 적용), 사이트 링크 연결 , 검색창 오류 수정, 상단 다크다운 문제 해결 , 로그인 페이지 수정

--- front_web/jsp/w3.js
+++ front_web/jsp/w3.js
@@ -15,3 +15,57 @@ window.addEventListener('load', function() {
         }
     });
 });
+
+/*(function() {
+    let onpageLoad = localStorage.getItem("theme") || "";
+    let element = document.body;
+    element.classList.add(onpageLoad);
+    document.getElementById("theme").textContent =
+      localStorage.getItem("theme") || "light";
+      localStorage.clear();
+  })();*/
+// This function makes website Dark mode and create alarm that indicates dark mode is started.
+// Using bootstrap's class "dark-mode" to make website dark mode.
+// dark mode is handled by localStorage's key "theme"
+// If localStorage's key "theme" is "dark", then dark mode is activated.
+function darkzzing(){
+    // Edit this variable to change the text of the alert.
+    const darkmode_alert_message = "Dark mode is activated. 다크 모드 활성화 되어짐.";
+  
+    let element = document.body;
+    // Toggle dark mode
+    element.classList.toggle("dark-mode");
+    // Toggles the "theme" in localStorage between "dark" and "normal"
+    localStorage.setItem("theme", element.classList.contains("dark-mode") ? "dark" : "normal");
+    // If dark mode is enabled, then create dark mode alert message
+    if(localStorage.getItem("theme") === "dark"){
+      // Creates frame
+      var alert_element = document.createElement("div");
+      alert_element.setAttribute("name", "dark-mode-alert");
+      alert_element.setAttribute("class", "alert-custom " + "alert-success " + "theme ");
+      // Creates <strong> element
+      var strong_element = document.createElement("strong");
+      // creates <i> element
+      var i_element = document.createElement("i");
+      i_element.setAttribute("class", "fa "+ "fa-exclamation-circle " +  "fa-lg ");
+      // creates <center> element
+      var center_element = document.createElement("center");
+      // Insert Text elements into <center> element
+      center_element.appendChild(document.createTextNode(darkmode_alert_message));
+      // insert <i>, <center> element into <strong>
+      strong_element.appendChild(i_element);
+      strong_element.appendChild(center_element);
+      // insert <strong> element into <div> element
+      alert_element.appendChild(strong_element);
+      // insert <div> element under the <header> element
+      document.getElementsByTagName("header")[0].appendChild(alert_element);
+    } else {
+      // If dark mode is disabled, then remove dark mode alert message.
+      // Alert message's name is "dark-mode-alert"
+      var alert_element = document.getElementsByName("dark-mode-alert");
+      // If alert message exists, then remove it.
+      if(alert_element.length > 0){
+        alert_element[0].remove();
+      }
+    }
+}
