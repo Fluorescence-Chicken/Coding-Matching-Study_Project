31a93d401b74e745d8fc3d6d76bc3ec0ce5e84c6
Mina-1316 <mina1316.canreply@gmail.com>
2022-05-26 07:14:29 +0900

    feat: signup integretion with backend API server
    회원가입 기능이 백엔드 API 서버와 연결되도록 자바스크립트 코드를 작성했습니다.

--- front_web/jsp/javascript.js
+++ front_web/jsp/javascript.js
@@ -1,9 +1,9 @@
 // scope for duck.nifskorea.com
 const duck_nifskorea_com = {
   //api_url 추가
-  api_url: "http://duck.nifskorea.com:8000/api/",
+  api_url: "http://duck.nifskorea.com:8000/api",
   login: {
-    _onLoginButton: function () {
+    onLoginButton: function () {
       // 값 들고오기
       const email = document.querySelector('input[name="input_email"]').value;
       const password = document.querySelector(
@@ -56,9 +56,48 @@ const duck_nifskorea_com = {
               alert("Error: " + user_xhr.status + ". Please contact to admin.");
             } // If - Else end
           };
-        } else { //로그인 실패 시
+        } else {
+          //로그인 실패 시
           alert(
             "Error: " + xhr.status + ". Please check your email and password."
+          );
+        }
+      };
+    },
+  },
+  signup: {
+    // signup webpage javascript scope
+    onSignUpButtonClick: function () {
+      // This function calls when signup button is clicked
+      // use XMLHttpRequest to call ajax request
+      // get user input data from form[name="signup_form"]
+      // define xhr and headers
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", `${duck_nifskorea_com.api_url}/auth/signup/`, true);
+      xhr.setRequestHeader("Content-Type", "application/json"); //json 보내기
+      xhr.setRequestHeader("Accept", "application/json"); //json 받기
+      // Get data from form
+      const data = {
+        username: document.querySelector('input[name="username"]').value,
+        email: document.querySelector('input[name="email"]').value,
+        password: document.querySelector('input[name="password"]').value,
+        first_name: document.querySelector('input[name="first_name"]').value,
+        last_name: document.querySelector('input[name="last_name"]').value,
+        address: document.querySelector('input[name="address"]').value,
+        job: document.querySelector('select[name="job"]').value,
+        gender: document.querySelector('select[name="gender"]').value,
+      };
+      xhr.send(JSON.stringify(data));
+      xhr.onload = () => {
+        // When the request is successful
+        if (xhr.status >= 200 || xhr.status < 300) {
+          alert(
+            "회원 가입이 성공했습니다! 입력한 아이디와 비밀번호로 로그인 하세요."
+          );
+          location.href = "/";
+        } else if (xhr.status === 400) {
+          alert(
+            "사용자 닉네임 또는 email에 중복이 있습니다. 기존의 아이디를 확인하거나, 새로운 아이디를 만들어 주세요. "
           );
         }
       };
