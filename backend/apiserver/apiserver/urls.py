428da16628a994c5362d7099419fecca59b8765a
Mina-1316 <mina1316.canreply@gmail.com>
2022-06-06 02:12:45 +0900

    docs: add swagger API interface
    Swagger API 인터페이스를 추가했습니다. 차후 모든 API view에 대해서 Documentation을 진행하시기 바랍니다.

--- backend/apiserver/apiserver/urls.py
+++ backend/apiserver/apiserver/urls.py
@@ -15,14 +15,40 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 from apiserver import settings
+
+
+schema_url_patterns = [
+    path('api/', include('api.urls')),
+]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="Coding Study Matching platform - Backend API",
+        default_version='api/',
+        description="클라우드프로그래밍 팀프로젝트 과제에 사용한 백엔드 API 입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    patterns=schema_url_patterns,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path(r'swagger(?P<format>\.json|\.yaml)', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 # add media url
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
